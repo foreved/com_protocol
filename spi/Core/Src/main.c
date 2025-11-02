1bf44f2574ef778a13dfb676c4770a41b9583e3c
foreved <foweved@qq.com>
2025-11-02 11:21:37 +0800

    finish

--- spi/Core/Src/main.c
+++ spi/Core/Src/main.c
@@ -67,7 +67,11 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
+  FATFS fs;           // 文件系统对象. 一般, 一个物理设备用一个 FATFS 描述
+  FIL file;           // 文件对象, 包含了文件的基本信息
+  FRESULT fres;       // 文件操作结果
+  UINT fnum;          // 文件成功读写数量
+  BYTE fbuffer[1024]; // 读写缓冲区
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -101,9 +105,82 @@ int main(void)
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOD);
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);
   /* USER CODE BEGIN 2 */
-  Lib_SPI_Init();
   Lib_USART_Init();
 
+  // 文件系统格式化
+  fres = f_mount(&fs, "0:", 1); // 将逻辑驱动器挂载到 FATFS
+  if (fres == FR_NO_FILESYSTEM) // 该逻辑驱动器还没有文件系统
+  {
+    Lib_USART_Send_String("Flash has no FAT. Initialize...\n");
+    // 创建文件系统
+    BYTE work[FF_MAX_SS]; // 创建文件系统需要工作缓冲区, 最少为 FF_MAX_SS
+    fres = f_mkfs("0:", (void*)0, work, FF_MAX_SS); // 使用默认参数初始化
+    if (fres == FR_OK) // 成功创建
+    {
+      Lib_USART_Send_String("FAT created sucessfully.\n");
+      fres = f_unmount("0:"); // 初始化文件系统后, 重新挂载
+      fres = f_mount(&fs, "0:", 0);
+    }
+    else // 创建失败
+    {
+      Lib_USART_Send_String("Error: fail to create FAT. Stop...\n");
+      while (1);
+    }
+  }
+  else if (fres != FR_OK) // 已创建文件系统, 但挂载失败
+  {
+    Lib_USART_Send_String("Error: fail to mount device. Stop...\n");
+    while (1);
+  }
+  else // 文件系统已创建, 且挂载成功
+  {
+    Lib_USART_Send_String("Succeed to mount device. Next to R/W.\n");
+  }
+
+  // 写测试
+  Lib_USART_Send_String("Now to test writing.\n");
+  // 以 "w" 模式打开文件
+  fres = f_open(&file, "0:/test.txt", FA_CREATE_ALWAYS | FA_WRITE);
+  if (fres == FR_OK) // 打开成功
+  {
+    const char str[] = "Hello world\nTest FatFs. Write\nRead.";
+    fres = f_write(&file, str, sizeof(str), &fnum);
+    if (fres == FR_OK)
+    {
+      Lib_USART_Send_fString("Succeed to write %d bytes.\n", fnum);
+      Lib_USART_Send_fString("The data is\n\t %s\n", str);
+    }
+    else
+    {
+      Lib_USART_Send_String("Error: fail to open the file. Stop...\n");
+      while (1);
+    }
+    f_close(&file);
+  }
+
+  // 读测试
+    Lib_USART_Send_String("Now to test reading.\n");
+  // 以 "r" 模式打开文件
+  fres = f_open(&file, "0:/test.txt", FA_OPEN_EXISTING | FA_READ);
+  if (fres == FR_OK) // 打开成功
+  {
+    fres = f_read(&file, fbuffer, sizeof(fbuffer), &fnum);
+    fbuffer[fnum] = '\0'; // f_read() 不会自动加 '\0'
+    if (fres == FR_OK)
+    {
+      Lib_USART_Send_fString("Succeed to read %d bytes.\n", fnum);
+      Lib_USART_Send_fString("The data is\n\t %s\n", fbuffer);
+    }
+    else
+    {
+      Lib_USART_Send_String("Error: fail to open the file. Stop...\n");
+      while (1);
+    }
+    f_close(&file);
+  }
+
+  f_unmount("0:");
+  Lib_USART_Send_String("Succeed to test Fatfs.\n");
 
   /* USER CODE END 2 */
 
